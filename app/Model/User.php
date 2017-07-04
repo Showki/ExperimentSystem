2e66f3fc80689938554379a5d528d4ebb5fd08f0
Showki <g231o026@s.iwate-pu.ac.jp>
2017-07-04 20:17:47 +0900

    passwordのバリデーション追加

--- app/Model/User.php
+++ app/Model/User.php
@@ -39,5 +39,18 @@ class User extends AppModel {
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 		),
+		'password' => array(
+			'notBlank' => array(
+				'rule' => array('notBlank'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+
+
+
 	);
 }
